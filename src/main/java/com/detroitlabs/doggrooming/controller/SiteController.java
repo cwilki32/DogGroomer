b1c2cf52617d280d0f19cc9e44bc9b26310c204c
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-04-11 12:53:13 -0600

    Adding base html and controller

--- src/main/java/com/detroitlabs/doggrooming/controller/SiteController.java
+++ src/main/java/com/detroitlabs/doggrooming/controller/SiteController.java
@@ -6,8 +6,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SiteController {
 
+    //home
+    //meet the staff
+    //services
+    //reviews
+    //faq
+    //contact
+    //book appt
     @RequestMapping("/")
     public String displayHomePage() {
         return "home";
+    }
+
+    @RequestMapping("/staff")
+    public String displayStaff() {
+        return "staff";
+    }
+    @RequestMapping("/services")
+    public String displayServices() {
+        return "services";
+    }
+    @RequestMapping("/reviews")
+    public String displayReviews() {
+        return "reviews";
+    }
+    @RequestMapping("/faq")
+    public String displayFAQ() {
+        return "faq";
+    }
+    @RequestMapping("/contact")
+    public String displayContact() {
+        return "contact";
+    }
+    @RequestMapping("/schedule")
+    public String displaySchedule() {
+        return "schedule";
     }
 }
