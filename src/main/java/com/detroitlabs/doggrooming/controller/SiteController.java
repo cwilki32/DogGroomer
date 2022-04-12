d2b46c6b9c3fac61ed4a883701efa54bd11e9bee
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-04-12 08:36:01 -0600

    Updating Controllers

--- src/main/java/com/detroitlabs/doggrooming/controller/SiteController.java
+++ src/main/java/com/detroitlabs/doggrooming/controller/SiteController.java
@@ -1,11 +1,19 @@
 package com.detroitlabs.doggrooming.controller;
 
+import com.detroitlabs.doggrooming.data.CompanyRepository;
+import com.detroitlabs.doggrooming.data.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SiteController {
 
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    ServiceRepository serviceRepository;
     //home
     //meet the staff
     //services
@@ -22,10 +30,13 @@ public class SiteController {
     public String displayStaff() {
         return "staff";
     }
+
     @RequestMapping("/services")
-    public String displayServices() {
+    public String displayServices(ModelMap modelMap) {
+        modelMap.put("services", ServiceRepository.getAllServices());
         return "services";
     }
+
     @RequestMapping("/reviews")
     public String displayReviews() {
         return "reviews";
@@ -34,10 +45,11 @@ public class SiteController {
     public String displayFAQ() {
         return "faq";
     }
-//    @RequestMapping("/contact")
-//    public String displayContact() {
-//        return "contact";
-//    }
+    @RequestMapping("/contact")
+    public String displayContact(ModelMap modelMap) {
+        modelMap.put("company", companyRepository.getCompanyInfo());
+        return "contact";
+    }
     @RequestMapping("/schedule")
     public String displaySchedule() {
         return "schedule";
