aab1fca7cf853c965b2745424f72361eec028a38
Amanda Vollmar <amanda.vollmar@detroitlabs.com>
2022-04-12 08:27:27 -0600

    create staff pages

--- src/main/java/com/detroitlabs/doggrooming/model/Staff.java
+++ src/main/java/com/detroitlabs/doggrooming/model/Staff.java
@@ -1,2 +1,37 @@
-package com.detroitlabs.doggrooming.model;public class Staff {
+package com.detroitlabs.doggrooming.model;
+
+public class Staff {
+    private String name;
+    private String imageID;
+    private String bio;
+
+    public Staff(String name, String imageID, String bio) {
+        this.name = name;
+        this.imageID = imageID;
+        this.bio = bio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(String imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
